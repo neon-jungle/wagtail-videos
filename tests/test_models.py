8f9d274f0f32a783d6d758ea417c78792eb6e108
Justin Crown <justincrown1@gmail.com>
2019-04-02 13:45:57 -0700

    fixing bug in updating file hash

--- tests/test_models.py
+++ tests/test_models.py
@@ -18,6 +18,11 @@ class TestVideoModel(WagtailTestUtils, TestCase):
         video.save()
         assert video.file_hash
         assert video.file_size
+        current_hash = video.file_hash
+        new_video_file = create_test_video_file(file_name='big_buck_bunny.mp4')
+        video.file = new_video_file
+        video.save()
+        assert video.file_hash != current_hash
 
     def test_create_file_hash_disabled(self):
         video_file = create_test_video_file()
