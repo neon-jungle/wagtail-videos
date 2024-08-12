65407529f9694f6ad940ce58d19aa2fd68365c28
Seb Brown <seb@neonjungle.studio>
2024-08-13 09:41:43 +1000

    Cleanup template + js

--- wagtailvideos/urls.py
+++ wagtailvideos/urls.py
@@ -8,9 +8,23 @@ urlpatterns = [
     path('add/', videos.add, name='add'),
     re_path(r'^usage/(\d+)/$', videos.usage, name='video_usage'),
 
-    path('multiple/add/', multiple.add, name='add_multiple'),
-    re_path(r'^multiple/(\d+)/delete/$', multiple.delete, name='delete_multiple'),
-    re_path(r'^multiple/(\d+)/$', multiple.edit, name='edit_multiple'),
+    path("multiple/add/", multiple.AddView.as_view(), name="add_multiple"),
+    path("multiple/<int:video_id>/", multiple.EditView.as_view(), name="edit_multiple"),
+    path(
+        "multiple/create_from_uploaded_image/<int:uploaded_file_id>/",
+        multiple.CreateFromUploadedVideoView.as_view(),
+        name="create_multiple_from_uploaded_image",
+    ),
+    path(
+        "multiple/<int:video_id>/delete/",
+        multiple.DeleteView.as_view(),
+        name="delete_multiple",
+    ),
+    path(
+        "multiple/delete_upload/<int:uploaded_file_id>/",
+        multiple.DeleteUploadView.as_view(),
+        name="delete_upload_multiple",
+    ),
 
     re_path(r'^(\d+)/delete/$', videos.delete, name='delete'),
     re_path(r'^(\d+)/create_transcode/$', videos.create_transcode, name='create_transcode'),
